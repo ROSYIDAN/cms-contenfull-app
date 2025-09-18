dc17e08e8ce0a480c91afdf58bd0ffb6f19900bd
SIDAN <rosidanfaisal@gmail.com>
2025-09-18 16:41:28 +0700

    Update Space Id

--- src/utils/entries.js
+++ src/utils/entries.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export const fetchData = async () => {
-  const spaceId = import.meta.env.VITE_SPACE_ID;
   const accessToken = import.meta.env.VITE_API_KEY;
   try {
     const res = await axios.get(
-      `https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}`
+      `https://cdn.contentful.com/spaces/${anv2u8ts9c4u}/entries?access_token=${accessToken}`
     );
     return res;
   } catch (error) {

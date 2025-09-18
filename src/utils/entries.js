df1c7a4f501563dbc456425547f9c87aeb60b8ec
SIDAN <rosidanfaisal@gmail.com>
2025-09-18 16:49:10 +0700

    Update Api Url

--- src/utils/entries.js
+++ src/utils/entries.js
@@ -4,7 +4,7 @@ export const fetchData = async () => {
   const accessToken = import.meta.env.VITE_API_KEY;
   try {
     const res = await axios.get(
-      `https://cdn.contentful.com/spaces/${anv2u8ts9c4u}/entries?access_token=${accessToken}`
+      `https://cdn.contentful.com/spaces/anv2u8ts9c4u/entries?access_token=${accessToken}`
     );
     return res;
   } catch (error) {
